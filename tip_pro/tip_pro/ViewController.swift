4ec0535ea4009ee3b82adf1033159459ebe18af1
juan hurtado <juanhurtado4@outlook.com>
2017-10-02 18:57:49 -0700

    Created interface functions

--- tip_pro/tip_pro/ViewController.swift
+++ tip_pro/tip_pro/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    @IBOutlet weak var tipAmountField: UITextField!
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
