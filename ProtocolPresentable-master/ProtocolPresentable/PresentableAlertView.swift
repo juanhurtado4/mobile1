0ae06cebb2006684be4b67df25f34cd73ba10e12
juan hurtado <juanhurtado4@outlook.com>
2017-10-19 12:35:54 -0700

    Implemented clousure to present an alert with an 'Ok' and 'cancel' button

--- ProtocolPresentable-master/ProtocolPresentable/PresentableAlertView.swift
+++ ProtocolPresentable-master/ProtocolPresentable/PresentableAlertView.swift
@@ -9,31 +9,59 @@
 import Foundation
 import UIKit
 
-// TODO:
+// COMPLETED: ELIEL'S TODO:
 // Write protocol  to present alert view on any UIViewController
 // Hint: You will need to constrain the protocol to UIViewController instances and use protocol extensions
+// ---------------------------------------------------------------------------------------------------
 
+// Mitchell's stretch challenges:
+// 1) add a second button that opens a different alert
+//      a) add a button in storyboard
+//      b) add a new IBAction
+//      c) add some code to the IBAction to open the alert
+// 2) Modify presentAlert() to take a cancelCallback that fires when cancel is pressed
+//      a) cancle callback could be optional
+//      b) cancelCallback: (()->())?
+//      c) cancelCallback?() or if cancelCallback != nil { cancelCallback() }
 
+// ---------------------------------------------------------------------------------------------------
 
-// Boilerplate for UIAlertController
-// 1. Alert ViewController
+//  NOTE TO SELF:
+// We are creating an protocol with an optional function
+@objc protocol AlertPresentable {
+     @objc optional func presentAlert()
+}
 
-//let alertVC = UIAlertController(
-//    title: "Some Tittle",
-//    message: "Some message",
-//    preferredStyle: .alert
-//)
-//
-//// 2. Adding Action to alertview
-//alertVC.addAction(
-//    UIAlertAction(
-//        title: "Ok",
-//        style: .default,
-//        handler: { (action) in
-//
-//    })
-//)
+// Below the extension defines the (presentAlert) function in the protocol where (alertPresentable) is of type (UIViewController)
+extension AlertPresentable where Self: UIViewController {
+    func presentAlert(title: String, message: String, okCallback: @escaping () -> ()) {
+        // NOTE: (UIAlertController is a class)
+        // Below alertVC is creating an instance of (UIAlertController)
+        let alertVC = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        // Now I am adding an action to the UIAlertController
+        alertVC.addAction(
+            // This creates an UIAlertAction object
+            UIAlertAction(
+                title: "Ok",            // set the title
+                style: .default,        // set the style
+                handler: { (action) in  // Adds functionality to the button once it is pressed
+                    okCallback()        // This callback passed in as a parameter from outside
+            }))
+        
+        // This adds an action (cancel) to this button. It does the exact same thing as above except
+        // it adds a "cancel" button instead of an "OK" button
+        alertVC.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // alertVC.addAction(UIAlertAction)
+        
+        self.present(alertVC, animated: true, completion: nil)
+    }
+}
 
-
-// 2. TODO: Present Alert View
-
+func test(cancelCallback:(()->())?) {
+    cancelCallback?()
+}
--- ProtocolPresentable-master/ProtocolPresentable/ViewController.swift
+++ ProtocolPresentable-master/ProtocolPresentable/ViewController.swift
@@ -6,12 +6,25 @@
 //  Copyright © 2017 MakeSchool. All rights reserved.
 //
 
+// Mitchell's stretch challenges:
+// 1) add a second button that opens a different alert
+//      a) add a button in storyboard
+//      b) add a new IBAction
+//      c) add some code to the IBAction to open the alert
+// 2) Modify presentAlert() to take a cancelCallback that fires when cancel is pressed
+//      a) cancle callback could be optional
+//      b) cancelCallback: (()->())?
+//      c) cancelCallback?() or if cancelCallback != nil { cancelCallback() }
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AlertPresentable {
 
     @IBAction func presentView(_ sender: Any) {
-        // TODO: Use your protocol function to present the alert view controller
+        // COMPLETED: ELIEL'S TODO: Use your protocol function to present the alert view controller
+        presentAlert(title: "King Jon Snow", message: "King of the north", okCallback: {
+            print("This prints when ok button was pressed")
+        })
     }
     override func viewDidLoad() {
         super.viewDidLoad()        
