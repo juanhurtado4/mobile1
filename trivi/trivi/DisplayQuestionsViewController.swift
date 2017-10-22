3bb80ab5652f9c659f6b37ec3e681e5e147e9be7
juan hurtado <juanhurtado4@outlook.com>
2017-10-22 12:14:23 -0700

    Created UI element for display questions VC

---
+++ trivi/trivi/DisplayQuestionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  trivi
+//
+//  Created by juan hurtado on 10/22/17.
+//  Copyright © 2017 make school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
