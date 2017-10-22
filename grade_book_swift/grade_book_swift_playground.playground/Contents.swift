ff8d2213d62a08735950e07aceb7aea2c14e394c
juan hurtado <juanhurtado4@outlook.com>
2017-10-22 10:54:26 -0700

    Finished project

--- grade_book_swift/grade_book_swift_playground.playground/Contents.swift
+++ grade_book_swift/grade_book_swift_playground.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class Student {
     
-    // MARK: PROPERTIES
+    // MARK: STUDENT PROPERTIES
     var name: String
     let studentId: Int
     var assignment = [String:  Int]()
@@ -12,26 +12,27 @@ class Student {
         self.studentId = studentId
     }
     
-        func addAssignment(assignment: String, grade: Int) {
-            /*
-             Function adds new assignment to the dictionary of assignments
-             Returns nil
-            */
-            self.assignment[assignment] = grade
-        }
-        
-        func getAssignment() -> [String: Int] {
-            /*
-             Function returns a dictionary of assignments
-            */
-            return self.assignment
-        }
+    // MARK: STUDENT FUNCTIONS
+    func addAssignment(assignment: String, grade: Int) {
+        /*
+         Function adds new assignment to the dictionary of assignments
+         Returns nil
+         */
+        self.assignment[assignment] = grade
+    }
+    
+    func getAssignment() -> [String: Int] {
+        /*
+         Function returns a dictionary of assignments
+         */
+        return self.assignment
+    }
     
     func deleteAssignment(assignment: String) {
         /*
          Function deletes assignment from dictionary of student assignments
          Returns nil
-        */
+         */
         self.assignment.removeValue(forKey: assignment)
     }
     
@@ -39,7 +40,7 @@ class Student {
         /*
          Function updates assignment
          Returns nil
-        */
+         */
         self.deleteAssignment(assignment: oldAssignment)
         self.addAssignment(assignment: newAssignmnet, grade: score)
     }
@@ -47,7 +48,7 @@ class Student {
     func getGpa() -> Int {
         /*
          Function returns the avg grade for all the assignments
-        */
+         */
         var result = 0
         for grade in self.assignment.values {
             result += grade
@@ -56,9 +57,92 @@ class Student {
     }
 }
 
+
+class Classroom {
+    // MARK: CLASSROOM PROPERTIES
+    var className: String
+    var roster = [Int: Student]()
+    
+    init(className: String) {
+        self.className = className
+    }
+    
+    // MARK: CLASSROOM FUNCTIONS
+    func getGpa() -> Int {
+        /*
+         Returns the avg grade for the entire class_name
+         based on each individual student grades
+        */
+        var final = 0
+        if self.roster.count != 0 {
+            for student in self.roster.values {
+                final += student.getGpa()
+            }
+            return final / roster.count
+    
+        }else {
+            return 0
+        }
+    }
+    
+    func addStudentAssignments(assignment: String, grade score: Int, studentId: Int? = nil) {
+        /*
+        Function adds student assignment to individual student if student id provide
+        If not it will add assignment to all students
+        Returns nil
+        */
+        
+        if let id = studentId {
+            self.roster[id]?.addAssignment(assignment: assignment, grade: score)
+        }else {
+            for student in self.roster.keys {
+                self.roster[student]?.addAssignment(assignment: assignment, grade: score)
+            }
+        }
+    }
+    
+    func deleteStudentAssignment(student: Student, assignment: String) {
+        /*
+         Functions deletes assignmentj
+         Returns nil
+        */
+        student.deleteAssignment(assignment: assignment)
+    }
+    
+    func addStudent(student: Student) {
+        /*
+        Function adds student to dictionary of roster
+        Returns nil
+        */
+        self.roster[student.studentId] = student
+    }
+    
+    func removeStudent(student: Student) {
+        /*
+         Function removes student from dictionary of roster
+         Returns nil
+         */
+        self.roster.removeValue(forKey: student.studentId)
+    }
+    
+}
+
+
+
+
+
+
+
+
+/* Testing
+var computerScience101 = Classroom(className: "cs1")
 var juan = Student(name: "juan", studentId: 1)
-juan.getAssignment()
-juan.assignment["grade_book"] = 10
-juan.assignment["simmulation"] = 10
-juan.assignment["moodTracker"] = 10
-juan.getGpa()
+var jose = Student(name: "jose", studentId: 2)
+
+juan.addAssignment(assignment: "gradeBook", grade: 85)
+jose.addAssignment(assignment: "gradeBook", grade: 85)
+
+computerScience101.addStudent(student: juan)
+computerScience101.addStudent(student: jose)
+computerScience101.getGpa()
+*/
